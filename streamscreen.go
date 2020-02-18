afa5f3116614dc86f8b15b9a082b4cefa8d29783
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-02-18 19:42:31 +0100

    Env parse
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- streamscreen.go
+++ streamscreen.go
@@ -12,7 +12,7 @@ import (
 func streamScreen() error {
 	nDisplays := screenshot.NumActiveDisplays()
 	display := 0
-	displayStr := os.Getenv("DISPLAY")
+	displayStr := os.Getenv("D")
 	if displayStr != "" {
 		disp, err := strconv.Atoi(displayStr)
 		if err != nil {
@@ -27,6 +27,61 @@ func streamScreen() error {
 	}
 	bounds := screenshot.GetDisplayBounds(display)
 	fmt.Printf("Display #%d bounds: %+v\n", display, bounds)
+	x := bounds.Min.X
+	y := bounds.Min.Y
+	w := bounds.Max.X
+	h := bounds.Max.Y
+	str := os.Getenv("X")
+	if str != "" {
+		v, err := strconv.Atoi(str)
+		if err != nil {
+			return err
+		}
+		if v >= 0 && v <= w {
+			x = v
+		}
+	}
+	str = os.Getenv("W")
+	if str != "" {
+		v, err := strconv.Atoi(str)
+		if err != nil {
+			return err
+		}
+		if v >= 0 && v <= w-x {
+			w = v
+		}
+	}
+	if w > bounds.Max.X-x {
+		w = bounds.Max.X - x
+	}
+	str = os.Getenv("Y")
+	if str != "" {
+		v, err := strconv.Atoi(str)
+		if err != nil {
+			return err
+		}
+		if v >= 0 && v <= h {
+			y = v
+		}
+	}
+	str = os.Getenv("H")
+	if str != "" {
+		v, err := strconv.Atoi(str)
+		if err != nil {
+			return err
+		}
+		if v >= 0 && v <= h-y {
+			h = v
+		}
+	}
+	if h > bounds.Max.Y-y {
+		h = bounds.Max.Y - y
+	}
+	bounds.Min.X = x
+	bounds.Min.Y = y
+	bounds.Max.X = w
+	bounds.Max.Y = h
+	fmt.Printf("Bounds: %+v\n", bounds)
 	return nil
 }
 
